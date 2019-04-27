336a3d6e38b508175b70f43d89856779c01fb552
gabouch <gabriel.deramecourt@gmail.com>
2019-04-27 16:05:16 +0200

    Init de Pygame

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+# Initialisation de pygame
+pygame.init()
+
+# Creation de la fenetre
+fenetre = pygame.display.set_mode((640, 400), RESIZABLE)
+
+# Controle de la boucle
+continuer = True
+
+while continuer:
+    pass
